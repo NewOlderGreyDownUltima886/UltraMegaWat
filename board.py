f2b733c78e0655e9691d3665fb4235364197d804
terra-soup <jordey.web@gmail.com>
2026-06-24 10:33:25 +1000

    board core

---
+++ board.py
@@ -0,0 +1,107 @@
+from enum import Enum
+
+class End(Enum):
+    ok = "1"
+    no = "2"
+    finished = "3"
+
+class Board:
+    def __init__(self, size, player_symbol_dict):
+        '''создание поля'''
+        x_size = size[0]
+        y_size = size[1]
+        self.board = [['' for j in range(x_size)] for i in range(y_size)]
+
+        '''инициализация игроков и символов'''
+        self.player_data = []
+        for i, n in enumerate(player_symbol_dict):
+            self.player_data.append([])
+            # порядковый номер
+            self.player_data[i].append(i+1)
+            # имя (передано извне)
+            self.player_data[i].append(n)
+            # символ (передано с именем извне)
+            self.player_data[i].append(player_symbol_dict.get(n))
+
+        self.player_symbols_list = []
+        for p, e in enumerate(self.player_data):
+            self.player_symbols_list.append(e[2])
+        
+            #print(self.board)
+            #print(self.player_data)
+    def go(self, x_cord_new, y_cord_new , going_player):
+        self.x_cord_new = x_cord_new
+        self.y_cord_new = y_cord_new
+        self.going_player = going_player
+
+
+        'проверка на вшивость'
+        if self.board[y_cord_new][x_cord_new] in self.player_symbols_list:
+            print(self.board)
+            print(self.board[y_cord_new][x_cord_new])
+            print('это менсто занято!')
+            End.no
+            return
+        'ход игрока'
+
+        # узнаем символ игрока по имени:
+        self.new_symbol = ''
+        for i, e in enumerate(self.player_data):
+            if going_player in e:
+                self.new_symbol = str(e[2])
+        # ходим
+        self.board[y_cord_new][x_cord_new] = self.new_symbol
+        
+        End.ok
+
+        # вывод поля
+        for i, e in enumerate(self.board):
+            print(str(e))
+
+    def check_finished(self):
+        # проверка по горизонтали
+        y_core = self.board[self.y_cord_new]
+        if len(set(y_core)) == 1 and y_core[0] != '': 
+            print(self.going_player + ' Победил!!! горизонталь')
+            End.finished
+
+        # проверка по вертикали
+        x_core = []
+        for i, n in enumerate(self.board):
+            x_core.append(n[self.x_cord_new])
+        if len(set(x_core)) == 1 and x_core[0] != '': 
+            print(self.going_player + ' Победил!!! вертикаль')
+            End.finished
+
+        # проверка по диагонали вверх
+        dia_up_core = []
+        for i, e in enumerate(self.board):
+            if i == self.y_cord_new:
+                for r, t in enumerate(self.board):
+                    dia_up_core.append(e[(-r)-1])
+        if len(set(dia_up_core)) == 1 and dia_up_core[0] != '': 
+            print(self.going_player + ' Победил!!! диагональ вверх')
+            End.finished
+        
+        # проверка по диагонали вниз
+        dia_down_core = []
+        for i, e in enumerate(self.board):
+            if i == self.y_cord_new:
+                for r, t in enumerate(self.board):
+                    dia_down_core.append(t[r])
+        if len(set(dia_down_core)) == 1 and dia_down_core[0] != '': 
+            print(self.going_player + ' Победил!!! диагональ вниз')
+            End.finished
+        
+game = Board([3, 3], player_symbol_dict={})
+game.go(x_cord_new=int(), y_cord_new=int(), going_player={}) # (x_cord_new, y_cord_new, 'Гриша')
+game.check_finished()
+
+
+'''game = Board([3, 3], player_symbol_dict={'Коля': '+', 'Гриша': '0', 'Андрей': '#'})
+while True:
+    x_cord_new = int(input('введите ход по х: '))
+    y_cord_new = int(input('введите ход по y: '))
+
+    game.go(x_cord_new, y_cord_new, 'Гриша')
+    game.check_finished()'''
